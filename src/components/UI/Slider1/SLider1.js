a228b21f0483a5d494b077e12150c68fc13b9016
ntnhat267 <90461453+ntnhat267@users.noreply.github.com>
2022-10-23 19:50:07 +0700

    fix slider

--- src/components/UI/Slider1/SLider1.js
+++ src/components/UI/Slider1/SLider1.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Col, Row } from 'reactstrap';
 
 import './Slider1.scss'
 
@@ -8,9 +9,15 @@ const SLider1 = ({slide}) => {
         <div className='slide'>
             <img src={slide.slide} className='img' alt={slide.slide}/>
             <div className='slide__bg' >
-                <div className='slide__bg--content'>
-                    <h2>Find Your Perfect Place To Stay</h2>
-                    <NavLink to='/room'>Book Now</NavLink>
+                <div className='slide__bg--content h-100'>
+                  
+                        <Row  className='h-100 align-items-center'>
+                            <Col lg='10' xl="8" md='12' className='offset-1'>
+                                <h2>Find Your Perfect Place To Stay</h2>
+                                <NavLink to='/room'>Book Now</NavLink>
+                            </Col>
+
+                        </Row>
                 </div>
             </div>
 
