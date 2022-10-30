2abf7735b7e355f9955c9479a9eda6e844799911
nhs21202 <90045608+nhs21202@users.noreply.github.com>
2022-10-30 23:11:52 +0700

    HomeExclusiveOffer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { Counter } from './features/counter/Counter';
 // import './App.css';
 import Layout from './components/Layout/Layout';
+import Services from './components/Services/Services'
+import ExclusiveOffer from 'components/ExclusiveOffer/ExclusiveOffer';
 
 function App() {
   return (
     <div className="App">
       <Layout/>
+      <ExclusiveOffer/>
     </div>
   );
 }
